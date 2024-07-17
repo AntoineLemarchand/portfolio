397c8658861444f320b571ae4045d39a62cfcdc5
alemarch <antoine.lemarchand@mailbox.org>
2024-07-17 22:32:01 +0200

    feat: centered about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -33,20 +33,28 @@ export default function About() {
     };
 
     return (
-        <div className="w-9/12 min-w-80 mx-auto">
-            <Tilt className="m-5 p-6 bg-fg w-fit h-80 flex flex-col items-center float-left" options={options}>
-                <Image className="pb-0 pe-0 bg-bg" src="/profile.svg" alt="Profile" width={200} height={200} />
-                <p className={`${delicious.className} text-black mt-4 rotate-3 text-3xl`}>
-                    Nice meeting you !
-                </p>
-            </Tilt>
-            <div className="inline">
-                <h3 className={`text-5xl text-fg`}>
-                    About me
-                </h3>
-                <p className='text-xl'>
-                    I am a software engineer based in France. I have a passion for concise, efficient, and maintainable code. I also like to play some bass and draw pixel art.
-                </p>
+        <div className="w-9/12 min-w-80 mx-auto h-screen flex justify-center items-center relative">
+            <div>
+                <Tilt className="w-44 m-5 p-3 bg-fg flex flex-col items-center float-left" options={options}>
+                    <Image className="ps-2 pt-2 bg-bg" src="/profile.svg" alt="Profile" width={150} height={150} />
+                    <p className={`${delicious.className} text-black mt-4 rotate-2 text-3xl text-center`}>
+                        Antoine Lemarchand
+                    </p>
+                </Tilt>
+                <div className="inline">
+                    <h3 className="text-4xl text-fg">
+                        About me
+                    </h3>
+                    <p className='text-xl mb-2'>
+                        I am a software engineer based in France. I have tend to appreciate, as one does, clean, maintainable and efficient code.
+                        I have a strong interest in both front-end and back-end development.
+                        I mainly use JavaScript and TypeScript framework, but I am sometimes working with other technologies (Golang, PHP...).
+                    </p>
+                    <p className='text-xl'>
+                        I like to work on personal projects, such as this website, to learn new technologies and improve my skills.
+                        I am also interested in music production and pixel art (or at least trying as you can see !).
+                    </p>
+                </div>
             </div>
         </div>
     );
